3aa6551b2bf4889baf9ba1b8e723cabe432896c0
sourav-jpg <souravpadmaraj6@gmail.com>
2023-02-17 18:00:49 +0530

    fetching all productsStatic

--- populate.js
+++ populate.js
@@ -16,8 +16,10 @@ const start = async () => {
     await Product.deleteMany();
     await Product.create(jsonProducts);
     console.log("Success!!!");
+    process.exit(0)
   } catch (error) {
-    console.log(error);
+    console.log(error)
+    process.exit(1)
   }
 };
 start();
--- server/controller/products.js
+++ server/controller/products.js
@@ -1,16 +1,32 @@
+const Product = require("../model/productSchema");
 
+const getAllproductsStatic = async (req, res, next) => {
+  let result;
+  try {
+    result = await Product.find( ).lean();
+    if (!result) {
+      res.send(400).json({
+        message: "Failed to fetch as there is nothing!!!",
+        error: true,
+        data: null,
+      });
+    }
+      res.status(200).json({
+      message: " All Products Fetched successfully!",
+      error: false,
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+    process.next();
+  }
+};
 
-const getAllproductsStatic = async( req, res, next) =>{
-  res.status(200).json({message:'products testing route'})
-}
-
-const getAllproducts = async( req, res, next) =>{
-    res.status(200).json({message:'products route'})
-
-}
+const getAllproducts = async (req, res, next) => {
+  res.status(200).json({ message: "products route" });
+};
 
 module.exports = {
-    getAllproductsStatic,
-    getAllproducts
-
-}
+  getAllproductsStatic,
+  getAllproducts,
+};
